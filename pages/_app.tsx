9c3abb0cea8cd397d2f42f20a88e3bf0f2c82fbf
elzup <guild0105@gmail.com>
2020-02-05 21:20:09 +0900

    install semantic ui

--- pages/_app.tsx
+++
@@ -1,8 +0,0 @@
-import { NextPage } from 'next'
-import { AppProps } from 'next/app'
-
-const App: NextPage<AppProps> = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />
-}
-
-export default App
