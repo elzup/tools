7212504310477cae012c7942383a76dbb5142ccd
elzup <guild0105@gmail.com>
2020-02-05 21:43:25 +0900

    add 404 page

---
+++ pages/_error.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import Layout from '../components/Layout'
+
+const Error = () => {
+  return (
+    <Layout title="GHA BadgeMaker">
+      <h1>404 Error</h1>
+    </Layout>
+  )
+}
+
+export default Error
