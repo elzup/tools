e812f7b123ec487549a86126f5bca5fe51764c9f
elzup <guild0105@gmail.com>
2020-02-07 17:06:40 +0900

    fotter: layout

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react'
 import Link from 'next/link'
 import Head from 'next/head'
+import { useRouter } from 'next/router'
 import { Container, List } from 'semantic-ui-react'
 
 type Props = {
@@ -22,32 +23,31 @@ const routings: Routing[] = [
   },
 ]
 
-const Layout: React.FC<Props> = ({
+const Layout: React.FC<Props & { currentPath: string }> = ({
   children,
   title = 'mini web tools by anozon',
+  currentPath,
 }) => (
-  <div>
-    <Head>
-      <title>{title}</title>
-      <meta charSet="utf-8" />
-      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-    </Head>
-    <header></header>
-    <Container>{children}</Container>
+  <div className="root">
+    <div>
+      <Head>
+        <title>{title}</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <header></header>
+      <Container>{children}</Container>
+    </div>
     <footer>
       <Container>
         <hr />
         <nav>
           <List>
             {routings.map(routing => (
-              <List.Item
+              <MenuItem
+                routing={routing}
+                opened={routing.path === currentPath}
                 key={routing.path}
-                icon={routing.icon}
-                content={
-                  <Link href={routing.path}>
-                    <a>{routing.label}</a>
-                  </Link>
-                }
               />
             ))}
           </List>
@@ -56,14 +56,56 @@ const Layout: React.FC<Props> = ({
       </Container>
     </footer>
     <style jsx>{`
-      nav {
-        display: flex;
-        div:not(:first-child) {
-          margin-left: 1rem;
+      .root {
+        padding: 2rem 0;
+        height: 100vh;
+        margin: 0;
+        display: grid;
+        grid-template-rows: 1fr auto;
+
+        .wrapper {
+          min-height: 100%;
+          margin-bottom: -50px;
+        }
+        footer {
+          grid-row-start: 2;
+          grid-row-end: 3;
         }
       }
     `}</style>
   </div>
 )
 
-export default Layout
+function MenuItem({ routing, opened }: { routing: Routing; opened: boolean }) {
+  return (
+    <>
+      <List.Item
+        icon={routing.icon}
+        data-opened={opened}
+        content={
+          opened ? (
+            routing.label
+          ) : (
+            <Link href={routing.path}>
+              <a>{routing.label}</a>
+            </Link>
+          )
+        }
+      />
+      <style jsx>{`
+        .item {
+          border: blue solid;
+          background: blue;
+        }
+      `}</style>
+    </>
+  )
+}
+
+function LayoutRouter(props: React.PropsWithChildren<Props>) {
+  const { pathname } = useRouter()
+
+  return <Layout {...props} currentPath={pathname} />
+}
+
+export default LayoutRouter
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import 'semantic-ui-css/semantic.min.css'
+import './global.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
