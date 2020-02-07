770c3dc0ce12a06daa44ad5da44cfb07dfbdd003
elzup <guild0105@gmail.com>
2020-02-07 17:07:04 +0900

    toppage

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react'
 import { NextPage } from 'next'
+import { Header, Icon } from 'semantic-ui-react'
 import Layout from '../components/Layout'
 
 const IndexPage: NextPage = () => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
-      <h1>Tools made by anozon</h1>
-      <p>Collection of Minimum web tools</p>
+      <Header as="h1" icon textAlign="center" style={{ marginTop: '40px' }}>
+        <Icon name="microchip" circular />
+        <Header.Content>
+          Tools
+          <p>made by anozon</p>
+        </Header.Content>
+      </Header>
+      <Header as="h3" icon textAlign="center">
+        Collection of Minimum web tools
+      </Header>
     </Layout>
   )
 }
