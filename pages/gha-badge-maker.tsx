86d0469992101bda37e4a031100742555d851caf
elzup <guild0105@gmail.com>
2020-02-07 18:20:47 +0900

    setup: jest

--- pages/gha-badge-maker.tsx
+++ pages/gha-badge-maker.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react'
-import { Input } from 'semantic-ui-react'
+import { Input, Header } from 'semantic-ui-react'
 import Layout from '../components/Layout'
 
+export function convertUrlToBadge(url: string, action?: string) {
+  const parts = url.split('/')
+  const tail = parts.pop()
+
+  const repoUrl = parts.join('/')
+  const actionName = action || tail?.split('%3A').pop()
+
+  const badgeUrl = `${repoUrl}/workflows/${action || actionName}/badge.svg`
+
+  return {
+    actionName,
+    badgeUrl,
+    badgeText: `![${actionName}](${badgeUrl})`,
+  }
+}
+
+const title = 'GHA BadgeMaker'
 const GHABadgePage = () => {
   const [url, setUrl] = React.useState<string>('')
 
   return (
-    <Layout title="GHA BadgeMaker">
-      <h1>Introduction</h1>
-      <h3>GHA BadgeMaker</h3>
+    <Layout title={title}>
+      <Header as="h1">{title}</Header>
       <p>Generate GitHub Actions Badge by url.</p>
       <Input
         value={url}
