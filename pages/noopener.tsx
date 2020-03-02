7437720283d810b53d84b249632ccd7a9faf5835
elzup <guild0105@gmail.com>
2020-03-02 09:40:58 +0900

    add noopener attacker

---
+++ pages/noopener.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import { Input, Header, Form } from 'semantic-ui-react'
+import Layout from '../components/Layout'
+
+const title = 'noopener検証'
+const NoOpener = () => {
+  return (
+    <Layout title={title}>
+      <Header as="h1">{title}</Header>
+      <p>rel=noopenerをつけていないリンク</p>
+
+      <p>
+        <a href="/noopener-attacker" target="_blank">
+          noopenerをつけていない危険なリンク
+        </a>
+      </p>
+      <p>
+        <a href="/noopener-attacker" target="_blank" rel="noopener">
+          noopenerをつけているリンク
+        </a>
+      </p>
+      <p>リンクを開いた状態でフォームに入力してください。</p>
+
+      <Form.Field>
+        <label></label>
+        <Input style={{ width: '100%' }} type="password" size="large" />
+      </Form.Field>
+    </Layout>
+  )
+}
+
+export default NoOpener
