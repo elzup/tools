8fe89227e438177efc23d0701e858099287be1e5
elzup <guild0105@gmail.com>
2020-05-07 17:15:12 +0900

    add google charts

--- pages/normal-distribution.tsx
+++ pages/normal-distribution.tsx
@@ -1,5 +1,8 @@
 import * as React from 'react'
-import { Input, Header, Message, Form, TextArea } from 'semantic-ui-react'
+import { Form, Header, Input, Label } from 'semantic-ui-react'
+import { Chart } from 'react-google-charts'
+// eslint-disable-next-line import/no-unresolved
+import { GoogleDataTableColumn } from 'react-google-charts/dist/types'
 import Layout from '../components/Layout'
 
 type ParseResult = {
@@ -8,42 +11,55 @@ type ParseResult = {
   badgeText: string
 } | null
 
-export function convertUrlToBadge(url: string, action?: string): ParseResult {
-  const parseReg = '(https://github.com/.*/.*)/actions\\?query=workflow%3A(.*)'
-  const m = new RegExp(parseReg).exec(url)
+const columns: GoogleDataTableColumn[] = [
+  {
+    label: 'num',
+    type: 'number',
+  },
+  {
+    label: 'label',
+    type: 'number',
+  },
+]
 
-  if (!m) return null
-
-  const [_, repoUrl, actionPath] = m
-  const actionName = action || actionPath
-  const badgeUrl = `${repoUrl}/workflows/${actionName}/badge.svg`
-
-  return {
-    actionName,
-    badgeUrl,
-    badgeText: `![${actionName}](${badgeUrl})`,
-  }
-}
-const exampleUrl =
-  'https://github.com/elzup/tools/actions?query=workflow%3Aqawolf'
+const rows = [
+  [2015, 5],
+  [2016, 3],
+  [2018, 1],
+]
 
 const title = '正規分布ツール'
 const NormalDistribution = () => {
-  const [url, setUrl] = React.useState<string>('')
+  const [mu, setMu] = React.useState<number>(2400)
+  const [sigma, setSigma] = React.useState<number>(140)
 
   return (
     <Layout title={title}>
       <Header as="h1">{title}</Header>
-      <p></p>
-      <Form.Field>
-        <Input
-          value={url}
+      <Form.Group widths="equal">
+        <Form.Input
+          label="μ"
+          value={sigma}
           style={{ width: '100%' }}
           size="large"
-          placeholder={exampleUrl}
-          onChange={({ target: { value } }) => setUrl(value)}
+          onChange={({ target: { value } }) => setSigma(Number(value))}
         />
-      </Form.Field>
+        <Form.Input
+          value={mu}
+          label="σ"
+          style={{ width: '100%' }}
+          size="large"
+          onChange={({ target: { value } }) => setMu(Number(value))}
+        />
+      </Form.Group>
+      <Chart
+        chartType="AreaChart"
+        width="100%"
+        height="400px"
+        legendToggle
+        rows={rows}
+        columns={columns}
+      />
     </Layout>
   )
 }
