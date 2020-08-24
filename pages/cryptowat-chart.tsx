1629b51b50663610744054dd380c80854fa3430a
elzup <guild0105@gmail.com>
2020-08-24 22:14:54 +0900

    data set receive

--- pages/cryptowat-chart.tsx
+++ pages/cryptowat-chart.tsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic'
 import * as React from 'react'
-import { Plot } from '../components/Graph'
+import { DataSet } from '../components/Graph'
 import Layout from '../components/Layout'
 
 const Graph = dynamic(() => import('../components/Graph'), { ssr: false })
@@ -8,32 +8,29 @@ const Graph = dynamic(() => import('../components/Graph'), { ssr: false })
 const title = 'Cryptowat Chart'
 
 const useCryptowat = () => {
-  const [plots, setPlots] = React.useState<Plot[]>([])
+  const [datasets, setDatasets] = React.useState<DataSet | null>(null)
 
   React.useEffect(() => {
     const socket = new WebSocket('ws://localhost:8000/all')
 
     socket.onmessage = ({ data }: { data: string }) => {
-      const rows: number[][] = JSON.parse(data)
-      const plots: Plot[] = rows.map(
-        (row): Plot => ({ time: new Date(row[0] * 1000), v: row[4] })
-      )
+      const res = JSON.parse(data) as DataSet
 
-      setPlots(plots)
+      setDatasets(res)
     }
     return () => socket.close()
   }, [])
 
-  return plots
+  return datasets
 }
 
 const Chart = () => {
-  const plots = useCryptowat()
+  const datasets = useCryptowat()
 
   return (
     <Layout title={title}>
       <p>{title}</p>
-      <Graph plots={plots}></Graph>
+      {datasets && <Graph datasets={datasets}></Graph>}
     </Layout>
   )
 }
