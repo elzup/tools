62236e6fbb61ae18f65c957c16b04008417fd7a5
elzup <guild0105@gmail.com>
2020-08-25 15:14:37 +0900

    fix draw graph froce

--- components/PixiComponents.tsx
+++ components/PixiComponents.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable new-cap */
 import { PixiComponent } from '@inlet/react-pixi'
 import { Graphics } from 'pixi.js'
 
@@ -6,13 +7,11 @@ type RectProps = {
   y: number
   width: number
   height: number
-  color?: number
+  color: number
 }
-
-// eslint-disable-next-line new-cap
 export const Rectangle = PixiComponent<RectProps, Graphics>('Rectangle', {
   create: () => new Graphics(),
-  applyProps: (instance, _, props) => {
+  applyProps: (instance, op, props) => {
     const { x, y, width, height, color } = props
 
     instance.clear()
