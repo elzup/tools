9eabb90ad3638c83ea46b3b0e1391d93af723b2e
elzup <guild0105@gmail.com>
2021-07-27 11:56:16 +0900

    init textmaster

---
+++ src/pages/textmaster.tsx
@@ -0,0 +1,49 @@
+import React, { useState } from 'react'
+import { Button } from 'semantic-ui-react'
+
+type Props = {}
+function Component(_props: Props) {
+  const [text, setText] = useState<string>('')
+  const [pretext, setPreText] = useState<null | string>(null)
+
+  return (
+    <div>
+      <Button
+        onMouseOver={() => setPreText(text.replace(/^\$\s+/gm, ''))}
+        onMouseLeave={() => setPreText(null)}
+        onClick={() => {
+          setText(text.replace(/^\$\s+/gm, ''))
+        }}
+      >
+        delete $
+      </Button>
+
+      <Button
+        onClick={() => {
+          setText(text.replace(/^[\s]+|[\s]+$/gm, ''))
+        }}
+      >
+        delete Spaces
+      </Button>
+
+      <textarea
+        value={text}
+        onChange={(e) => {
+          setText(e.target.value)
+        }}
+      ></textarea>
+      <pre style={{ background: pretext ? '#ddd' : '#aaa' }}>
+        {(pretext || text).split('').map((c, i) => (
+          <span key={`${c}_${i}`}>{c}</span>
+        ))}
+      </pre>
+      <style jsx>{`
+        span {
+          border-bottom: 1px solid #ccc;
+        }
+      `}</style>
+    </div>
+  )
+}
+
+export default Component
