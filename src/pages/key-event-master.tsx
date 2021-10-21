6e88b8b2eae326a656f6b7fa31d421dce11c833c
elzup <guild0105@gmail.com>
2021-10-21 12:35:11 +0900

    add key event listener

--- src/pages/key-event-master.tsx
+++ src/pages/key-event-master.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react'
+import React, { useState } from 'react'
 import { Header } from 'semantic-ui-react'
+import { useKeyPressEvent } from 'react-use'
 import Layout from '../components/Layout'
 import { useKeyQueue } from '../components/useKey'
 
@@ -12,7 +13,37 @@ const KeyEventMaster = () => {
       <Header as="h1">{title}</Header>
       <p>press: {pressQueue.join(',')}</p>
       <p>changedPress: {changePressQueue.join(',')}</p>
+      <Demo />
     </Layout>
+  )
+}
+
+const Demo = () => {
+  const [count, setCount] = useState(0)
+
+  const increment = () => setCount((count) => count + 1)
+  const decrement = () => setCount((count) => count - 1)
+  const reset = () => setCount(() => 0)
+
+  useKeyPressEvent(
+    () => true,
+    (e) => {
+      console.log(e.key)
+      console.log('pressed')
+    },
+    increment
+  )
+  useKeyPressEvent('x', decrement, decrement)
+  useKeyPressEvent('r', reset)
+
+  return (
+    <div>
+      <p>
+        Try pressing <code>[</code>, <code>]</code>, and <code>r</code> to see
+        the count incremented and decremented.
+      </p>
+      <p>Count: {count}</p>
+    </div>
   )
 }
 
