c56f4899b3f50eb40f0e40cb8311bf3c53269bbc
elzup <guild0105@gmail.com>
2021-10-25 11:51:33 +0900

    refactor, add ref

--- src/components/KeyDemo.tsx
+++ src/components/KeyDemo.tsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import { useKeyQueue } from './useKey'
+import React, { useState } from 'react'
+import { useKeyQueue, useRefKey } from './useKey'
 
 const KeyDemo = () => {
   const { downQueue, upQueue, downAllQueue } = useKeyQueue()
+  const [press, setPress] = useState<string>('leaved')
+  const ref = useRefKey<HTMLDivElement>(
+    ({ key }) => setPress(key),
+    () => 'leaved'
+  )
 
   return (
     <div>
-      <p>downQueue: {downQueue.join(',')}</p>
-      <p>upQueue: {upQueue.join(',')}</p>
-      <p>downAllQueue: {downAllQueue.join(',')}</p>
+      <div>
+        <p>downQueue: {downQueue.join(',')}</p>
+        <p>upQueue: {upQueue.join(',')}</p>
+        <p>downAllQueue: {downAllQueue.join(',')}</p>
+      </div>
+      <div ref={ref} style={{ border: 'solid 1px' }}>
+        ref area
+        <p>{press}</p>
+      </div>
     </div>
   )
 }
