5a021f7e0d7b93adc7ce8a1dc3a3f9021a711c72
elzup <guild0105@gmail.com>
2022-01-09 21:15:55 +0900

    add font

--- src/pages/divergence-meter.tsx
+++ src/pages/divergence-meter.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import React, { useState } from 'react'
 import { Header } from 'semantic-ui-react'
 import DivergenceMeterComp from '../components/DivergenceMeter'
@@ -11,6 +12,15 @@ const DivergenceMeter = () => {
 
   return (
     <Layout title={title}>
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+
+        <link
+          href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Header as="h1">{title}</Header>
       <input
         // pattern="[0-9.]{0,12}"
