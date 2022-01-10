20dd95f23e2df76f084ef8ee346ac064db7811e8
elzup <guild0105@gmail.com>
2022-01-11 00:36:38 +0900

    first svg animation

--- src/pages/svg-play.tsx
+++ src/pages/svg-play.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import { Header } from 'semantic-ui-react'
+import styled from 'styled-components'
 import Layout from '../components/Layout'
 
 const W = 1280
@@ -18,7 +19,10 @@ const Ground = () => {
       strokeLinecap="round"
       strokeLinejoin="round"
     >
-      <path d="M19 12H6M12 5l-7 7 7 7" />
+      <g id="g1">
+        <circle id="c1" cx={100} cy={100} r={100} />
+        <circle id="c2" cx={200} cy={140} r={100} />
+      </g>
     </svg>
   )
 }
@@ -28,9 +32,26 @@ const SvgPlay = () => {
   return (
     <Layout title={title}>
       <Header as="h1">{title}</Header>
-      <Ground />
+      <Wrap>
+        <Ground />
+      </Wrap>
     </Layout>
   )
 }
+
+const Wrap = styled.div`
+  border: gray solid 1px;
+  #g1 {
+    animation: move 10s infinite;
+  }
+  @keyframes move {
+    0% {
+      transform: translateX(0);
+    }
+    100% {
+      transform: translateX(100%);
+    }
+  }
+`
 
 export default SvgPlay
