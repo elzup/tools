da435b9ffceec24f326a2f16ab2806d7d1f511a3
elzup <guild0105@gmail.com>
2022-01-12 10:25:43 +0900

    fix font-size

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -2,9 +2,5 @@ import { Typography } from '@mui/material'
 import React from 'react'
 
 export const Title: React.FC = ({ children }) => {
-  return (
-    <Typography variant="h3" component="h1">
-      {children}
-    </Typography>
-  )
+  return <Typography component="h1">{children}</Typography>
 }
