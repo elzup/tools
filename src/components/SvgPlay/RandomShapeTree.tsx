c691b52ea8cc57120cd894a2a298d753b9228b64
elzup <guild0105@gmail.com>
2022-01-19 00:31:52 +0900

    update anim

--- src/components/SvgPlay/RandomShapeTree.tsx
+++ src/components/SvgPlay/RandomShapeTree.tsx
@@ -1,14 +1,23 @@
 import { random, range, sample } from 'lodash'
 import { useMemo } from 'react'
-import { Circle, Fan, Props as ShapeProps, Rect, RectInCircle } from './Shape'
+import { Circle, Donut, Fan, PadCircle, Rect, SmallRect } from './Shape'
 
 type Props = {
   depthLimit: number
   w: number
+  force?: Partial<ShapeItem>
 }
 
-const shapes = ['circle', 'fan', 'rect', 'rectInCircle'] as const
-const anims = ['spin', 'stay'] as const
+const shapes = [
+  'circle',
+  'fan',
+  'rect',
+  'smallRect',
+  'donut',
+  'padCircle',
+] as const
+// const anims = ['stay'] as const
+const anims = ['spin', 'stay', 'move'] as const
 
 type Shape = typeof shapes[number]
 type Anime = typeof anims[number]
@@ -22,22 +31,32 @@ const positions: Record<ChildPos, Pos> = {
   center: { sx: 0, sy: 0 },
 } as const
 
+const randomShapeRating = {
+  1: ['circle', 'fan', 'rect', 'smallRect'],
+  2: ['circle', 'fan', 'rect', 'smallRect', 'donut', 'padCircle'],
+  3: ['circle', 'fan', 'rect', 'smallRect', 'donut', 'padCircle'],
+  4: ['circle', 'fan', 'rect'],
+}
+
 type ShapeItem = {
   pos: Pos
   shape: Shape
   anime: Anime
   childNum: number
+  deg: number
 }
 
-export const RandomShapeTree = ({ depthLimit, w }: Props) => {
+const RandomShapeTree = ({ force, depthLimit, w }: Props) => {
   const item: ShapeItem = useMemo(() => {
     return {
       pos: sample(Object.values(positions)) || { sx: 0, sy: 0 },
       shape: sample(shapes) || 'circle',
       anime: sample(anims) || 'stay',
       childNum: random(1, 4),
+      deg: random(360),
+      ...force,
     }
-  }, [])
+  }, [force])
 
   if (depthLimit === 0) {
     return null
@@ -48,8 +67,8 @@ export const RandomShapeTree = ({ depthLimit, w }: Props) => {
 
   return (
     <svg x={wx} y={wy}>
-      <rect x={-w / 2} y={-w / 2} stroke="orange" width={w} height={w} />
-      <g className={item.anime}>
+      {/* <rect x={-w / 2} y={-w / 2} stroke="orange" width={w} height={w} /> */}
+      <g className={item.anime} style={{ transform: `rotate(${item.deg}deg)` }}>
         <RandomShapeDraw shape={item.shape} w={w} />
         {range(item.childNum).map((i) => (
           <RandomShapeTree key={i} w={w / 2} depthLimit={depthLimit - 1} />
@@ -67,9 +86,15 @@ export const RandomShapeDraw = ({ shape, w }: { shape: Shape; w: number }) => {
       return <Fan w={w} />
     case 'rect':
       return <Rect w={w} />
-    case 'rectInCircle':
-      return <RectInCircle w={w} />
+    case 'smallRect':
+      return <SmallRect w={w} />
+    case 'padCircle':
+      return <PadCircle w={w} />
+    case 'donut':
+      return <Donut w={w} />
     default:
       return null
   }
 }
+
+export default RandomShapeTree
