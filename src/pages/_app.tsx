50ac353d47f53b306654da84b34d131ecddd282e
elzup <guild0105@gmail.com>
2022-01-20 10:51:50 +0900

    update _document

---
+++ src/pages/_app.tsx
@@ -0,0 +1,25 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { CssBaseline } from '@mui/material'
+import { ThemeProvider } from '@emotion/react'
+import { GlobalStyle, theme } from '../components/theme'
+
+const App = ({ Component, pageProps }: AppProps) => (
+  <>
+    <Head>
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, shrink-to-fit=no"
+      />
+      {/* <link rel="shortcut icon" href="/images/icon.png" /> */}
+      {/* <link rel="manifest" href="/manifest.json" /> */}
+    </Head>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <GlobalStyle />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  </>
+)
+
+export default App
