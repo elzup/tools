aef1d3066c2d5fb2a198111d75b40288eed02d45
elzup <guild0105@gmail.com>
2022-02-20 23:24:29 +0900

    add wordleTryable

--- src/__tests__/wordle.test.ts
+++ src/__tests__/wordle.test.ts
@@ -1,9 +1,14 @@
-import { WordleAnswerResult, wordleCheck } from '../utils/wordle'
+import {
+  WordleAnswerResult,
+  wordleCheck,
+  wordleTryable,
+  LibChecker,
+} from '../utils/wordle'
 
 const readable = (res: WordleAnswerResult) =>
   res.chars.map(({ char, judge: result }) => `${char} (${result})`).join(' ')
 
-describe('wordle', () => {
+describe('wordleCheck', () => {
   it('allOk', () => {
     const res = wordleCheck('abcde', 'abcde')
 
@@ -28,5 +33,33 @@ describe('wordle', () => {
     expect(readable(res)).toMatchInlineSnapshot(
       `"く (nea) ろ (non) い (non) あ (non) か (hit)"`
     )
+  })
+})
+
+const lib: LibChecker = {
+  あいうえお: true,
+  かき: true,
+  くけこ: true,
+  さしす: true,
+  せそ: true,
+}
+
+describe('wordleTryable', () => {
+  it('match 5', () => {
+    expect(wordleTryable('あいうえお', lib)).toBe(true)
+  })
+  it('match 2,3', () => {
+    expect(wordleTryable('かきくけこ', lib)).toBe(true)
+    expect(wordleTryable('せそくけこ', lib)).toBe(true)
+  })
+  it('match 3,2', () => {
+    expect(wordleTryable('さしすせそ', lib)).toBe(true)
+    expect(wordleTryable('さしすかき', lib)).toBe(true)
+  })
+  it('non match', () => {
+    expect(wordleTryable('さしすてと', lib)).toBe(false)
+    expect(wordleTryable('たちつてと', lib)).toBe(false)
+    expect(wordleTryable('えおあいう', lib)).toBe(false)
+    expect(wordleTryable('さしすけこ', lib)).toBe(false)
   })
 })
--- src/utils/wordle.ts
+++ src/utils/wordle.ts
@@ -1,3 +1,5 @@
+import { Dict } from '../types'
+
 const resultTypes = ['non', 'nea', 'hit'] as const
 
 export type CharJudge = typeof resultTypes[number]
@@ -33,4 +35,21 @@ export const wordleCheck = (
   return { chars, allOk: chars.every(({ judge }) => judge === 'hit') }
 }
 
-// export const wordleTryable = (answer: string) => {}
+export type LibWord = {
+  text: string
+  raeding: string
+}
+
+export type LibChecker = Dict<true>
+export type LibWordMap = Dict<LibWord>
+
+export const wordleTryable = (answer: string, lib: LibChecker) => {
+  const head2 = answer.substring(0, 2)
+  const head3 = answer.substring(0, 3)
+  const tail2 = answer.substring(3, 5)
+  const tail3 = answer.substring(2, 5)
+
+  return Boolean(
+    lib[answer] || (lib[head2] && lib[tail3]) || (lib[head3] && lib[tail2])
+  )
+}
