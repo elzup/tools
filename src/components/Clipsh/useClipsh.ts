9cda4553cc4d350652d2a463a3842da7d95cece8
elzup <guild0105@gmail.com>
2022-02-28 00:17:59 +0900

    update clipsh hooks

---
+++ src/components/Clipsh/useClipsh.ts
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react'
+import { readClipboard, writeClipboard } from '../../lib/clipboard'
+import { useTequery } from './useTequery'
+
+export const useClipsh = () => {
+  const [query, setQuery] = useState<string>('')
+  const [base, setBase] = useState<string>('')
+
+  useEffect(() => {
+    readClipboard().then(setBase)
+  }, [])
+  const teq = useTequery(base, query)
+
+  return {
+    query,
+    setQuery,
+    teq,
+    base,
+    saveText: () => {
+      writeClipboard(teq.result)
+    },
+  }
+}
--- src/components/Clipsh/useTequery.ts
+++ src/components/Clipsh/useTequery.ts
@@ -1,14 +1,5 @@
-import { useMemo, useState } from 'react'
+import { useMemo } from 'react'
 import { tequery } from 'tequery'
-import { readClipboard } from '../../lib/clipboard'
 
-export const useTeQuery = () => {
-  const [query, setQuery] = useState<string>('')
-  const base = useMemo(readClipboard, [])
-
-  const result = useMemo(() => {
-    return tequery(base, query)
-  }, [query, base])
-
-  return { query, setQuery, result, base }
-}
+export const useTequery = (base: string, query: string) =>
+  useMemo(() => tequery(base, query), [query, base])
