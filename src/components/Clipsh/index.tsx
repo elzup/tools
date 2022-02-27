ab5ce1a1659b5bdb2e7e78e3934f13f6700362ad
elzup <guild0105@gmail.com>
2022-02-27 23:52:52 +0900

    add clipsh page

---
+++ src/components/Clipsh/index.tsx
@@ -0,0 +1,18 @@
+import { Box } from '@mui/material'
+import React from 'react'
+import styled from 'styled-components'
+
+function ClipshContent() {
+  return (
+    <Style>
+      <Box style={{ marginLeft: '8px' }}></Box>
+      <code>
+        <pre>{''}</pre>
+      </code>
+    </Style>
+  )
+}
+
+const Style = styled.div``
+
+export default ClipshContent
