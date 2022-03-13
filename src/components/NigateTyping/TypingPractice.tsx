b0b502063cddd942b180fde67de48e594c9b65b3
elzup <guild0105@gmail.com>
2022-03-13 17:29:22 +0900

    create typing

---
+++ src/components/NigateTyping/TypingPractice.tsx
@@ -0,0 +1,78 @@
+import { asciify, range } from '@elzup/kit'
+import React from 'react'
+import styled from 'styled-components'
+
+type Props = {
+  word: string
+}
+function TypingPractice({ word }: Props) {
+  const ans = range(10)
+    .map(() => word)
+    .join(' ')
+  const [text, setText] = React.useState('')
+  const onChange = (text: string) => {
+    if (text === ans) {
+      return setText('')
+    }
+    if (text.length > ans.length) return
+    setText(text)
+  }
+
+  return (
+    <Style>
+      <div className="answer">
+        {ans.split('').map((c, i) => (
+          <span
+            key={i}
+            data-correct={c === text[i]}
+            data-current={i === text.length}
+            data-leached={i < text.length}
+          >
+            {c}
+          </span>
+        ))}
+      </div>
+      <div className="in">
+        <input
+          onChange={(e) => onChange(asciify(e.currentTarget.value))}
+          value={text}
+        />
+      </div>
+    </Style>
+  )
+}
+
+const Style = styled.div`
+  border: solid 1px #ccc;
+  border-radius: 4px;
+  padding: 0.4rem 0.2rem;
+  .answer {
+    margin-left: 4px;
+    span {
+      color: gray;
+      &[data-correct='true'] {
+        color: black;
+      }
+      &[data-correct='false'][data-leached='true'] {
+        color: red;
+        border-bottom: solid 1px red;
+      }
+      &[data-current='true'] {
+        border-bottom: solid 1px blue;
+        background: hsla(180, 50%, 50%, 0.5);
+      }
+    }
+  }
+
+  .in input {
+    width: 100%;
+  }
+
+  .answer span,
+  .in input {
+    font-family: 'Roboto Mono', monospace;
+    font-size: 100%;
+  }
+`
+
+export default TypingPractice
--- src/components/NigateTyping/index.tsx
+++ src/components/NigateTyping/index.tsx
@@ -1,32 +1,53 @@
-import { Box, Container, TextField } from '@mui/material'
+import { Box, Container, TextField, Typography } from '@mui/material'
 import React from 'react'
 import styled from 'styled-components'
+import { asciify } from '../../utils'
 import { useLocalStorage } from '../../utils/useLocalStorage'
+import TypingPractice from './TypingPractice'
 
 function useNigateWords() {
   const [words, setWords] = useLocalStorage<string>('nigate-words', '')
 
-  return {
-    words,
-    setWords,
-  }
+  return { words, setWords }
 }
 
 function NigateTyping() {
   const { words, setWords } = useNigateWords()
+  const practiceWords = words.split('\n').filter(Boolean)
 
   return (
     <Style>
       <Container>
-        <Box mt={2}>
-          <TextField
-            label="練習リスト"
-            multiline
-            onChange={(e) => {
-              setWords(e.currentTarget.value)
-            }}
-            value={words}
-          />
+        <Box m={1}>
+          <Typography>
+            半角文字のみです。改行区切りです。保存されます。スコアなどはありません。
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'grid',
+            gridTemplateColumns: 'max-content 1fr',
+            gap: '1rem',
+          }}
+        >
+          <Box mt={2}>
+            <TextField
+              label="練習リスト"
+              multiline
+              onChange={(e) => {
+                setWords(asciify(e.currentTarget.value))
+              }}
+              value={words}
+            />
+          </Box>
+          <Box mt={2} style={{ display: 'grid', gap: '12px' }}>
+            {practiceWords.length === 0 && (
+              <Typography>←練習する単語を登録してください</Typography>
+            )}
+            {practiceWords.map((word, i) => (
+              <TypingPractice key={i} word={word} />
+            ))}
+          </Box>
         </Box>
       </Container>
     </Style>
