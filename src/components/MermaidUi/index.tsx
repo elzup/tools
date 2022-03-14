010c62f0c07774b088ce7daa1635bcda6d55f034
elzup <guild0105@gmail.com>
2022-03-14 23:51:18 +0900

    add useBlockGraph

--- src/components/MermaidUi/index.tsx
+++ src/components/MermaidUi/index.tsx
@@ -1,24 +1,52 @@
-import { TextField } from '@mui/material'
-import React, { useState } from 'react'
+import { TextField, Typography } from '@mui/material'
+import React, { useMemo, useState } from 'react'
 import MmdGraph from '../MmdGraph'
 import { useFetch } from './useFetch'
 
-// const config = {
-//   startOnLoad: true,
-//   flowchart: {
-//     useMaxWidth: true,
-//     htmlLabels: true,
-//     curve: 'cardinal',
-//   },
-//   securityLevel: 'loose',
-// }
+type GraphBlock = {
+  title: string
+  mmd: string
+}
+
+function useBlocks(text?: string): GraphBlock[] {
+  return useMemo(() => {
+    if (!text) return []
+
+    const allLines = text.split('\n')
+    const l = allLines.length
+
+    const { blocks } = allLines.reduce(
+      ({ blocks, lines }, line, i) => {
+        const isLast = i === l - 1
+        const title = line.match(/%%%subgraph (.*?);?$/)?.[1]
+
+        if (isLast || !!title) {
+          if (isLast) lines.push(line)
+          return {
+            blocks: [...blocks, { title: title || '', mmd: lines.join('\n') }],
+            lines: ['flowchart LR;'] as string[],
+          }
+        }
+        return { blocks, lines: [...lines, line] }
+      },
+      {
+        blocks: [
+          { title: 'sample', mmd: 'flowchart LR;\n\tA->B\n\tB->C\n' },
+        ] as GraphBlock[],
+        lines: [] as string[],
+      }
+    )
+
+    return blocks
+  }, [text])
+}
 
 function MurmaidUi() {
   const [url, setUrl] = useState<string>(
     'https://raw.githubusercontent.com/elzup/story-plots/main/sample/min-flow.mmd'
   )
   const { data, error: _error } = useFetch(url)
-  const mmd = data?.split('\n').slice(0, 1000).join('\n')
+  const blocks = useBlocks(data)
 
   return (
     <div>
@@ -31,7 +59,13 @@ function MurmaidUi() {
           setUrl(e.currentTarget.value)
         }}
       />
-      {mmd && <MmdGraph mmd={mmd} />}
+      {blocks.map((block, i) => (
+        <div key={`${i}_${block.title}`}>
+          <Typography variant="h5">{block.title}</Typography>
+
+          <MmdGraph mmd={block.mmd} />
+        </div>
+      ))}
     </div>
   )
 }
