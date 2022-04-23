9d1807aa7736205c77084145c452a55050a9a54d
elzup <guild0105@gmail.com>
2022-04-23 16:17:49 +0900

    add dictionaries color

--- src/components/Clipsh/useClipsh.ts
+++ src/components/Clipsh/useClipsh.ts
@@ -1,24 +1,32 @@
 import { useEffect, useState } from 'react'
 import { readClipboard, writeClipboard } from '../../lib/clipboard'
-import { useTequery } from './useTequery'
+import { useLocalStorage } from '../../utils/useLocalStorage'
+import { useTequeryMeta } from './useTequery'
 
 export const useClipsh = () => {
   const [query, setQuery] = useState<string>('')
   const [base, setBase] = useState<string>('')
+  const [history, pushHistory] = useLocalStorage<string[]>('clipsh-history', [])
 
   useEffect(() => {
     readClipboard().then(setBase)
   }, [])
-  const teq = useTequery(base, query)
+
+  const { teq, meta } = useTequeryMeta(base, query)
+
+  // const  = useTequery(base, query)
 
   return {
     query,
     setQuery,
     teq,
+    meta,
     base,
     setBase,
     beforeLn: base.split('\n').length,
     afterLn: teq.result.length,
+    history,
+    pushHistory,
     saveText: () => {
       writeClipboard(teq.result)
     },
--- src/components/Clipsh/useTequery.ts
+++ src/components/Clipsh/useTequery.ts
@@ -3,3 +3,15 @@ import { tequery } from 'tequery'
 
 export const useTequery = (base: string, query: string) =>
   useMemo(() => tequery(base, query), [query, base])
+
+export const useTequeryMeta = (base: string, query: string) => {
+  const teq = useTequery(base, query)
+  const meta = useMemo(() => {
+    return {
+      len: tequery(base, 'len'),
+      lineNum: tequery(base, 'lineNum'),
+    }
+  }, [base])
+
+  return { teq, meta }
+}
