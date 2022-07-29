f507dd9602faa6718fcac2636e93bc68e3b524f8
elzup <guild0105@gmail.com>
2022-07-29 10:48:06 +0900

    add ByteBlock

--- src/components/CodeExplorer/index.tsx
+++ src/components/CodeExplorer/index.tsx
@@ -1,13 +1,33 @@
+import { range } from '@elzup/kit'
 import { TextField } from '@mui/material'
 import React from 'react'
 import { useLocalStorage } from '../../utils/useLocalStorage'
 
 type Props = {}
 
+const uints = (b: Buffer) => [
+  ...range(b.byteLength).map((i) => {
+    return b.readUint8(i)
+  }),
+]
+
+const bitStr = (n: number) => n.toString(2).padStart(8, '0')
+
+const ByteBlock = ({ c }: { c: number }) => {
+  return (
+    <div>
+      <div>{c}</div>
+      <div>{bitStr(c)}</div>
+      <div>{c.toString(16).padStart(2, '0')}</div>
+    </div>
+  )
+}
+
 function CodeExplorer(props: Props) {
   const [text, setText] = useLocalStorage<string>('code-explorer-text', '')
 
   const buf = Buffer.from(text)
+  const intNums = uints(buf)
 
   return (
     <div>
@@ -19,6 +39,11 @@ function CodeExplorer(props: Props) {
         onChange={(e) => setText(e.currentTarget.value)}
       />
       <div>
+        <div style={{ display: 'flex' }}>
+          {intNums.map((v, i) => (
+            <ByteBlock key={i} c={v} />
+          ))}
+        </div>
         <pre>
           <code>{buf.length}</code>
         </pre>
