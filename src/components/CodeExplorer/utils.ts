5a892451329f3e74ffd071a6892bcda47e067b38
elzup <guild0105@gmail.com>
2022-07-31 22:53:10 +0900

    add format parser

--- src/components/CodeExplorer/utils.ts
+++ src/components/CodeExplorer/utils.ts
@@ -5,3 +5,25 @@ export const uints = (b: Buffer) => [
     return b.readUint8(i)
   }),
 ]
+
+const cmdChars1 = 'xcbB?'
+const cmdChars2 = 'hHe'
+const cmdChars4 = 'iIlLf'
+const cmdChars8 = 'qQd'
+
+export const cmdChars = `${cmdChars1}${cmdChars2}${cmdChars4}${cmdChars8}`
+
+// not supported: n N s p P
+
+export type Cmd = typeof cmdChars[number]
+
+export const isCmd = (cmd: string): cmd is Cmd => ''.includes(cmd)
+
+export const getFormat = (cmd: string) => {
+  if (!isCmd(cmd)) return null
+  if (cmdChars1.includes(cmd)) return { cmd, len: 1 }
+  if (cmdChars2.includes(cmd)) return { cmd, len: 2 }
+  if (cmdChars4.includes(cmd)) return { cmd, len: 4 }
+  if (cmdChars8.includes(cmd)) return { cmd, len: 8 }
+  return null
+}
