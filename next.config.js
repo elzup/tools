7a0f66307d5ae952c975f9f5364bc38f10bad20e
elzup <guild0105@gmail.com>
2022-11-04 13:28:20 +0900

    update config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 const withPWA = require('next-pwa')
+const runtimeCaching = require('next-pwa/cache')
+
 module.exports = withPWA({
   pwa: {
-    dest: 'public',
+    // dest: 'public',
+    runtimeCaching,
     disable: process.env.NODE_ENV === 'development',
+    buildExcludes: [/middleware-manifest.json$/],
   },
 })
