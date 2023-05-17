e26e3d5b3d24b222953df4f1cbb20e76de6b6d5d
elzup <guild0105@gmail.com>
2023-05-17 16:35:10 +0900

    add time

---
+++ src/lib/time/useAnotime.ts
@@ -0,0 +1,50 @@
+import { range } from '@elzup/kit/lib/range'
+import { useMemo } from 'react'
+
+const YR = 100
+const WR = 10
+const DR = 1
+
+export const useAnotime = () => {
+  const date = new Date()
+  const y = date.getFullYear()
+  const m = date.getMonth() + 1
+  const d = date.getDate()
+  const h = date.getHours()
+
+  const { buffer, positions } = useMemo(() => {
+    const times = range(100).map((v) => new Date(+date - v * 1000 * 60 * 60))
+    const positions = times.map((t) =>
+      calcRad(t.getFullYear(), t.getMonth() + 1, t.getDate(), t.getHours())
+    )
+    const buffer = new Float32Array(positions.flat())
+
+    return { buffer, positions }
+  }, [y, m, d, h])
+
+  return { positions, buffer, size: positions.length }
+}
+
+const isUru = (y: number) => {
+  return (y % 4 === 0 && y % 100 !== 0) || y % 400 === 0
+}
+const is31Month = (m: number) => ((m >> 3) & 1) ^ (m & 1)
+
+const monthDayNum = (y: number, m: number) =>
+  m === 2 ? (isUru(y) ? 29 : 28) : is31Month(m) ? 31 : 30
+const totalDayNum = (y: number, m: number) =>
+  range(m - 1)
+    .map((v) => monthDayNum(y, v + 1))
+    .reduce((a, b) => a + b, 0)
+
+export const calcRad = (year: number, mon: number, d: number, h: number) => {
+  const all = isUru(year) ? 366 : 365
+  const dm = totalDayNum(year, mon)
+  const perYear = (dm + (d - 1 + h / 24)) / all
+  const x = Math.sin(perYear * Math.PI * 2) * YR + YR
+  const y = Math.cos(perYear * Math.PI * 2) * YR + YR
+
+  const z = 0
+
+  return [x, y, z]
+}
--- src/pages/time.tsx
+++ src/pages/time.tsx
@@ -1,24 +1,22 @@
-import { Canvas, useFrame } from '@react-three/fiber'
-import { useMemo, useRef, useState } from 'react'
-import { Mesh, Vector3Tuple } from 'three'
+import { OrthographicCamera } from '@react-three/drei'
+import { Canvas } from '@react-three/fiber'
+import { Suspense, useRef } from 'react'
 import Layout from '../components/Layout'
 import { Title } from '../components/Title'
+import { useAnotime } from '../lib/time/useAnotime'
 
 function Spring() {
   const lineRef = useRef<SVGLineElement>(null)
-  const points: Vector3Tuple[] = [
-    [-10, 0, 0],
-    [0, 10, 0],
-    [10, 0, 0],
-  ]
-  const buffer = useMemo(() => new Float32Array(points.flat()), [...points])
+  const { buffer, size, positions } = useAnotime()
+
+  console.log(positions)
 
   return (
     <line ref={lineRef}>
       <bufferGeometry>
         <bufferAttribute
           attach="attributes-position"
-          count={points.length}
+          count={size}
           array={buffer}
           itemSize={3}
         />
@@ -28,48 +26,20 @@ function Spring() {
   )
 }
 
-function Box(props: { position: Vector3Tuple }) {
-  // This reference gives us direct access to the THREE.Mesh object
-  const ref = useRef<Mesh>(null)
-  // Hold state for hovered and clicked events
-  const [hovered, hover] = useState(false)
-  const [clicked, click] = useState(false)
-  // Subscribe this component to the render-loop, rotate the mesh every frame
-
-  useFrame((state, delta) => {
-    if (!ref.current) return
-    ref.current.rotation.x += delta
-  })
-  // Return the view, these are regular Threejs elements expressed in JSX
-
-  return (
-    <mesh
-      {...props}
-      ref={ref}
-      scale={clicked ? 1.5 : 1}
-      onClick={(e) => click(!clicked)}
-      onPointerOver={(e) => hover(true)}
-      onPointerOut={(e) => hover(false)}
-      position={[0, 0, 0]}
-    >
-      <boxGeometry args={[10, 2, 2]} />
-      <meshPhysicalMaterial color={hovered ? 'hotpink' : 'orange'} />
-      <bufferGeometry></bufferGeometry>
-    </mesh>
-  )
-}
-
 const title = 'Time'
 const Time = () => {
   return (
     <Layout title={title}>
       <Title>{title}</Title>
       <Canvas>
-        <ambientLight />
-        <Spring />
+        <Suspense fallback={null}>
+          <ambientLight />
+          <Spring />
 
-        <pointLight position={[10, 10, 10]} />
-        <Box position={[-1.2, 0, 0]} />
+          <pointLight position={[10, 10, 10]} />
+          {/* <Rig /> */}
+        </Suspense>
+        <OrthographicCamera makeDefault position={[0, 0, 10]} zoom={40} />
       </Canvas>
     </Layout>
   )
