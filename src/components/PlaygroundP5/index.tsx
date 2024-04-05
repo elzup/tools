a2757242f58ee11277c69a73b65ab6cced3ba207
elzup <guild0105@gmail.com>
2024-04-05 10:20:34 +0900

    spirographs

--- src/components/PlaygroundP5/index.tsx
+++ src/components/PlaygroundP5/index.tsx
@@ -2,16 +2,10 @@ import styled from 'styled-components'
 import P5Wrapper from './P5Wrapper'
 import { sketch1 } from './sketch1'
 import { sketchPu } from './sketchPu'
-import SixteenSegDisplay from './Seg16'
-import { SegTitle } from './SegTitle'
 
 const PlaygroundP5 = () => {
   return (
     <Style>
-      <SegTitle />
-      <SixteenSegDisplay char="1" />
-      <SixteenSegDisplay char="D" />
-      <SixteenSegDisplay char="A" />
       <P5Wrapper sketch={sketchPu} />
       <P5Wrapper sketch={sketch1} />
     </Style>
