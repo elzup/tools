ecfccc224b696776f773e350f925b699dd2e827c
elzup <guild0105@gmail.com>
2025-01-11 22:22:13 +0900

    init page

---
+++ src/components/MillionLearnTech/index.tsx
@@ -0,0 +1,17 @@
+import { Typography } from '@mui/material'
+import { Box } from '../common/mui'
+
+const MillionLearnTech = () => {
+  return (
+    <div>
+      <Box sx={{ display: 'grid' }}>
+        <Box>
+          <Typography>i18n 国際化</Typography>
+          Elena
+        </Box>
+      </Box>
+    </div>
+  )
+}
+
+export default MillionLearnTech
