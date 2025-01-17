b521fb4083c6e824464b72eda85bc7aaa9a8c293
elzup <guild0105@gmail.com>
2025-01-17 23:57:50 +0900

    add view

--- src/components/MillionLearnTech/MlEmily.tsx
+++ src/components/MillionLearnTech/MlEmily.tsx
@@ -2,18 +2,21 @@ import { MenuItem, Select } from '@mui/material'
 import { useState } from 'react'
 import { Card } from './Card'
 
-const t = {
+const t: Record<string, Record<string, string>> = {
   日本語: {
     title: 'エミリー',
     description: '国際化',
+    milionArmer: 'ミリオンアーマー',
   },
   英語: {
     title: 'Emily',
     description: 'Internationalization',
+    milionArmer: 'Million Armer',
   },
   エミリー: {
     title: 'エミリー',
     description: '国際化',
+    milionArmer: '百万装甲',
   },
 }
 
@@ -29,6 +32,9 @@ export const MlEmily = () => {
         <MenuItem value="英語">英語</MenuItem>
         <MenuItem value="エミリー">エミリー</MenuItem>
       </Select>
+      <p>{t[lang].title}</p>
+      <p>{t[lang].description}</p>
+      <p>{t[lang].milionArmer}</p>
     </Card>
   )
 }
