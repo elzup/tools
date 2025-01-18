f924e2302a7650d46f51d3266a02dedc2dd45d27
elzup <guild0105@gmail.com>
2025-01-18 23:59:06 +0900

    fix langs

--- src/components/MillionLearnTech/MlEmily.tsx
+++ src/components/MillionLearnTech/MlEmily.tsx
@@ -1,40 +1,47 @@
 import { MenuItem, Select } from '@mui/material'
 import { useState } from 'react'
+import { Box } from '../common/mui'
 import { Card } from './Card'
 
 const t: Record<string, Record<string, string>> = {
-  日本語: {
+  ja: {
     title: 'エミリー',
     description: '国際化',
     milionArmer: 'ミリオンアーマー',
   },
-  英語: {
+  en: {
     title: 'Emily',
     description: 'Internationalization',
     milionArmer: 'Million Armer',
   },
-  エミリー: {
-    title: 'エミリー',
+  emi: {
+    title: 'エミリー語',
     description: '国際化',
     milionArmer: '百万装甲',
   },
 }
 
 export const MlEmily = () => {
-  const [lang, setLang] = useState<string>('日本語')
+  const [lang, setLang] = useState<string>('ja')
 
   return (
     <Card>
       <h2>Emily</h2>
       <p>国際化</p>
+
       <Select value={lang} onChange={(e) => setLang(e.target.value)}>
-        <MenuItem value="日本語">日本語</MenuItem>
-        <MenuItem value="英語">英語</MenuItem>
-        <MenuItem value="エミリー">エミリー</MenuItem>
+        <MenuItem value="ja">日本語</MenuItem>
+        <MenuItem value="en">英語</MenuItem>
+        <MenuItem value="emi">エミリー</MenuItem>
       </Select>
-      <p>{t[lang].title}</p>
-      <p>{t[lang].description}</p>
-      <p>{t[lang].milionArmer}</p>
+
+      <Box>
+        <Card>
+          <h3>{t[lang].title}</h3>
+          <p>{t[lang].description}</p>
+          <p>{t[lang].milionArmer}</p>
+        </Card>
+      </Box>
     </Card>
   )
 }
