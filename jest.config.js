bc3748b1a42d21da34febeb563bb985dff0ea67f
elzup <guild0105@gmail.com>
2025-10-17 01:09:59 +0900

    fix test

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,16 @@
 module.exports = {
-  testEnvironment: 'node',
+  testEnvironment: 'jsdom',
   transform: {
-    '^.+\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
   },
-  testPathIgnorePatterns: ['.qawolf/', 'e2e'],
+  testPathIgnorePatterns: ['.qawolf/', 'e2e', '.next/', 'node_modules/'],
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json'],
+  transformIgnorePatterns: [
+    '/node_modules/',
+    '^.+\\.module\\.(css|sass|scss)$',
+  ],
+  moduleNameMapper: {
+    '^.+\\.module\\.(css|sass|scss)$': 'identity-obj-proxy',
+  },
+  watchman: false,
 }
