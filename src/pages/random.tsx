95db47f8e22e64e98ec106f7681928acac3fd390
elzup <guild0105@gmail.com>
2025-10-19 02:33:38 +0900

    fix pages

--- src/pages/random.tsx
+++ src/pages/random.tsx
@@ -1,11 +1,10 @@
 import { makeRand } from '@elzup/kit/lib/rand/make'
+import dynamic from 'next/dynamic'
 import Layout from '../components/Layout'
 import { Title } from '../components/Title'
-import dynamic from 'next/dynamic'
 
 const title = 'random inspect'
 
-const randBasic = Math.random
 const gen = () => {
   const r = makeRand(String(Date.now())).fn
 
@@ -15,16 +14,32 @@ const gen = () => {
     return ((tn ?? 0) % 1000000) / 1000000
   }
 }
+
+const randBasic = Math.random
 const randOrigin = gen()
 const funcs = [randBasic, randOrigin]
+
+const RandInspectContent = () => {
+  const randoms = funcs.map((func) => func())
+
+  return (
+    <>
+      {randoms.map((value) => (
+        <div key={value}>{value}</div>
+      ))}
+    </>
+  )
+}
+
+const RandInspectDynamic = dynamic(() => Promise.resolve(RandInspectContent), {
+  ssr: false,
+})
 
 const RandInspect = () => {
   return (
     <Layout title={title}>
       <Title>{title}</Title>
-      {funcs.map((func, i) => (
-        <div key={i}>{func()}</div>
-      ))}
+      <RandInspectDynamic />
     </Layout>
   )
 }
