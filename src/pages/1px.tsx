5b66d3a315d98a776d769cc32d8a88f4109b560f
elzup <guild0105@gmail.com>
2026-01-06 02:32:19 +0900

    fix 1px render

--- src/pages/1px.tsx
+++ src/pages/1px.tsx
@@ -1,10 +1,10 @@
 import { TextField, Typography } from '@mui/material'
-import React, { useEffect, useState } from 'react'
+import dynamic from 'next/dynamic'
+import React, { useState } from 'react'
 import { RGBColor, SketchPicker } from 'react-color'
 import { defaultPresetColors } from '../components/constants/color'
 import Layout from '../components/Layout'
 import { Title } from '../components/Title'
-import { getComponentHtmlCode } from '../utils'
 
 function gen1pxDataUrl(rgb: RGBColor) {
   const size = 1
@@ -23,6 +23,10 @@ function gen1pxDataUrl(rgb: RGBColor) {
   return canvasElem.toDataURL()
 }
 
+function gen1pxSvgCode(colorStr: string) {
+  return `<svg xmlns="http://www.w3.org/2000/svg" width="1" height="1" style="background: ${colorStr}"></svg>`
+}
+
 const checkerUrl =
   'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAYAAAAf8/9hAAAAMUlEQVQ4T2NkYGAQYcAP3uCTZhw1gGGYhAGBZIA/nYDCgBDAm9BGDWAAJyRCgLaBCAAgXwixzAS0pgAAAABJRU5ErkJggg=='
 
@@ -39,86 +43,85 @@ export const Svg1px = ({ color }: { color: string }) => {
   )
 }
 
-const Px1 = () => {
+const Px1Content = () => {
   const [hex, setHex] = useState<RGBColor>({ r: 0, g: 0, b: 0, a: 1 })
-  const [url, setUrl] = useState<string>('')
-  const [svgCode, setSvgCode] = useState<string>('')
 
   const colorStr = `rgba(${hex.r}, ${hex.g}, ${hex.b}, ${hex.a})`
-
-  const component = <Svg1px color={colorStr} />
-
-  useEffect(() => {
-    setUrl(gen1pxDataUrl(hex))
-
-    getComponentHtmlCode(component).then((code) => {
-      setSvgCode(code)
-    })
-  }, [hex])
-
+  const url = gen1pxDataUrl(hex)
   const imgUrl = `<img src="${url}" />`
+  const svgCode = gen1pxSvgCode(colorStr)
   const svgUrl = `data:image/svg+xml;utf8,${encodeURIComponent(svgCode)}`
 
   return (
+    <div style={{ display: 'grid' }}>
+      <SketchPicker
+        presetColors={[...defaultPresetColors, 'rgba(0, 0, 0, 0)']}
+        color={hex}
+        onChange={(e) => setHex(e.rgb)}
+      />
+
+      <Typography variant="h4">png</Typography>
+      <div
+        style={{
+          padding: '8px',
+          backgroundImage: `url(${checkerUrl})`,
+          width: 'fit-content',
+        }}
+      >
+        <img style={{ width: '40px' }} src={url} />
+      </div>
+      <TextField
+        multiline
+        value={url}
+        style={{ background: '#eee', width: '100%' }}
+      />
+      <TextField
+        multiline
+        value={imgUrl}
+        style={{ background: '#eee', width: '100%' }}
+      />
+
+      <Typography variant="h4">svg</Typography>
+      <div
+        style={{
+          padding: '8px',
+          backgroundImage: `url(${checkerUrl})`,
+          width: 'fit-content',
+        }}
+        dangerouslySetInnerHTML={{ __html: svgCode.replace(/"1"/g, '"40"') }}
+      ></div>
+      <div
+        style={{
+          padding: '8px',
+          backgroundImage: `url(${checkerUrl})`,
+          width: 'fit-content',
+        }}
+      >
+        <img style={{ width: '40px' }} src={svgUrl} />
+      </div>
+      <TextField
+        multiline
+        value={svgUrl}
+        style={{ background: '#eee', width: '100%' }}
+      />
+      <TextField
+        multiline
+        value={svgCode}
+        style={{ background: '#eee', width: '100%' }}
+      />
+    </div>
+  )
+}
+
+const Px1ContentDynamic = dynamic(() => Promise.resolve(Px1Content), {
+  ssr: false,
+})
+
+const Px1 = () => {
+  return (
     <Layout title={title}>
       <Title>{title}</Title>
-
-      <div style={{ display: 'grid' }}>
-        <SketchPicker
-          presetColors={[...defaultPresetColors, 'rgba(0, 0, 0, 0)']}
-          color={hex}
-          onChange={(e) => setHex(e.rgb)}
-        />
-        <Typography variant="h4">png</Typography>
-        <div
-          style={{
-            padding: '8px',
-            backgroundImage: `url(${checkerUrl})`,
-            width: 'fit-content',
-          }}
-        >
-          <img style={{ width: '40px' }} src={url} />
-        </div>
-        <TextField
-          multiline
-          value={url}
-          style={{ background: '#eee', width: '100%' }}
-        />
-        <TextField
-          multiline
-          value={imgUrl}
-          style={{ background: '#eee', width: '100%' }}
-        />
-
-        <Typography variant="h4">svg</Typography>
-        <div
-          style={{
-            padding: '8px',
-            backgroundImage: `url(${checkerUrl})`,
-            width: 'fit-content',
-          }}
-          dangerouslySetInnerHTML={{ __html: svgCode.replace(/"1"/g, '40') }}
-        ></div>
-        <div
-          style={{
-            padding: '8px',
-            backgroundImage: `url(${checkerUrl})`,
-            width: 'fit-content',
-          }}
-        >
-          <img style={{ width: '40px' }} src={svgUrl} />
-        </div>
-        <TextField
-          multiline
-          value={svgUrl}
-          style={{ background: '#eee', width: '100%' }}
-        />
-        <TextField
-          multiline
-          value={svgCode}
-          style={{ background: '#eee', width: '100%' }}
-        />
-      </div>
+      <Px1ContentDynamic />
     </Layout>
   )
 }
