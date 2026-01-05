bab91c81367af7a97d0c7707cd0f2100148804c3
elzup <guild0105@gmail.com>
2026-01-06 02:33:11 +0900

    fix ssr styleSheet

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,7 @@
 import createEmotionServer from '@emotion/server/create-instance'
 import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
 import React from 'react'
+import { ServerStyleSheet } from 'styled-components'
 import { theme } from '../components/theme'
 import createEmotionCache from '../utils/createEmotionCache'
 
@@ -27,35 +28,42 @@ class Document extends NextDocument {
 }
 
 Document.getInitialProps = async (ctx) => {
+  const sheet = new ServerStyleSheet()
   const originalRenderPage = ctx.renderPage
 
   const cache = createEmotionCache()
   const { extractCriticalToChunks } = createEmotionServer(cache)
 
-  ctx.renderPage = () =>
-    originalRenderPage({
-      enhanceApp: (App) => (props) => <App {...props} />,
-    })
+  try {
+    ctx.renderPage = () =>
+      originalRenderPage({
+        enhanceApp: (App) => (props) =>
+          sheet.collectStyles(<App {...props} />),
+      })
 
-  const initialProps = await NextDocument.getInitialProps(ctx)
+    const initialProps = await NextDocument.getInitialProps(ctx)
 
-  const emotionStyles = extractCriticalToChunks(initialProps.html)
-  const emotionStyleTags = emotionStyles.styles.map((style) => (
-    <style
-      data-emotion={`${style.key} ${style.ids.join(' ')}`}
-      key={style.key}
-      // eslint-disable-next-line react/no-danger
-      dangerouslySetInnerHTML={{ __html: style.css }}
-    />
-  ))
+    const emotionStyles = extractCriticalToChunks(initialProps.html)
+    const emotionStyleTags = emotionStyles.styles.map((style) => (
+      <style
+        data-emotion={`${style.key} ${style.ids.join(' ')}`}
+        key={style.key}
+        // eslint-disable-next-line react/no-danger
+        dangerouslySetInnerHTML={{ __html: style.css }}
+      />
+    ))
 
-  return {
-    ...initialProps,
-    styles: [
-      ...React.Children.toArray(initialProps.styles),
-      ...emotionStyleTags,
-    ],
-    emotionStyleTags,
+    return {
+      ...initialProps,
+      styles: [
+        ...React.Children.toArray(initialProps.styles),
+        sheet.getStyleElement(),
+        ...emotionStyleTags,
+      ],
+      emotionStyleTags,
+    }
+  } finally {
+    sheet.seal()
   }
 }
 
