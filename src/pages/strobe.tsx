0eab5ab33efc127daf7463a8204887a2e65dc1b3
elzup <guild0105@gmail.com>
2026-06-09 00:33:51 +0900

    fix: ストロボ効果ページのタイトルから補足括弧を削除

--- src/pages/strobe.tsx
+++ src/pages/strobe.tsx
@@ -6,7 +6,7 @@ const StrobeWheel = dynamic(() => import('../components/StrobeWheel'), {
   ssr: false,
 })
 
-const title = 'ストロボ効果 (回転が止まって見える)'
+const title = 'ストロボ効果'
 
 const StrobePage = () => {
   return (
